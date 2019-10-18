68fd3da4f1531698738d840a77039de9d928b768
Jordon de Hoog <jordon.dehoog@gmail.com>
2019-10-18 09:08:56 -0400

    bump the version

--- buildSrc/src/main/java/App.kt
+++ buildSrc/src/main/java/App.kt
@@ -4,7 +4,7 @@ object App {
 
     private const val major: Int = 1
     private const val minor: Int = 0
-    private const val patch: Int = 0
+    private const val patch: Int = 1
     private const val suffix: String = "-dev"
 
     const val rootName: String = "DFM Test"
